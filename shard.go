65508145861557698c3598a6805bad299009fc32
Wojciech Małota-Wójcik <59281144+wojciech-malota-wojcik@users.noreply.github.com>
2021-11-03 17:53:24 +0100

    Drain messages from NATS before exiting (#17)

--- shard.go
+++ shard.go
@@ -34,85 +34,81 @@ func runLocalShard(i uint64, rx <-chan interface{}, tx chan<- interface{}) paral
 
 		users := userList{}
 
-		for {
-			select {
-			case <-ctx.Done():
-				return ctx.Err()
-			case msg := <-rx:
-				log := log.With(zap.Any("message", msg))
-				switch m := msg.(type) {
-				case wire.AlarmStatusChanged:
-					alarms := users[m.UserID]
-					if alarms == nil {
-						alarms = alarmList{}
-						users[m.UserID] = alarms
-					}
-					alarm := alarms[m.AlarmID]
-					if alarm == nil {
-						alarm = &alarmStatus{}
-						alarms[m.AlarmID] = alarm
-					}
-
-					if alarm.LatestChangedAt.After(m.ChangedAt) {
-						log.Info("Update ignored because newer one exists")
-						continue
-					}
-					alarm.LatestChangedAt = m.ChangedAt
-
-					switch {
-					case alarm.Status != m.Status:
-						alarm.Status = m.Status
-						if alarm.Status == wire.StatusCleared {
-							log.Info(fmt.Sprintf("Status is %s, alarm won't be sent", alarm.Status))
-							alarm.ToSend = false
-						} else {
-							log.Info("Alarm triggered")
-							alarm.ToSend = true
-						}
-					case alarm.ToSend:
-						log.Info("Status hasn't changed, alarm was triggered earlier")
-					default:
-						log.Info("Status hasn't changed, alarm won't be sent")
-					}
-				case wire.SendAlarmDigest:
-					log := log.With(zap.Any("userID", m.UserID))
-
-					alarms := users[m.UserID]
-					if alarms == nil {
-						log.Info("No alarms for user, nothing to send")
-					}
-
-					active := &wire.AlarmDigest{
-						UserID: m.UserID,
-					}
-					var sent []*alarmStatus
-					for alarmID, alarm := range alarms {
-						if alarm.ToSend {
-							active.ActiveAlarms = append(active.ActiveAlarms, wire.Alarm{
-								AlarmID:         alarmID,
-								Status:          alarm.Status,
-								LatestChangedAt: alarm.LatestChangedAt,
-							})
-							sent = append(sent, alarm)
-						}
-					}
-
-					sort.Slice(active.ActiveAlarms, func(i int, j int) bool {
-						return active.ActiveAlarms[i].LatestChangedAt.Before(active.ActiveAlarms[j].LatestChangedAt)
-					})
-
-					tx <- active
-
-					for _, alarm := range sent {
-						alarm.ToSend = false
-					}
-
-					log.Info("Alarms sent", zap.Any("alarms", active))
-
-				default:
-					panic(fmt.Errorf("message of unknown type %T received", msg))
+		for msg := range rx {
+			log := log.With(zap.Any("message", msg))
+			switch m := msg.(type) {
+			case wire.AlarmStatusChanged:
+				alarms := users[m.UserID]
+				if alarms == nil {
+					alarms = alarmList{}
+					users[m.UserID] = alarms
 				}
+				alarm := alarms[m.AlarmID]
+				if alarm == nil {
+					alarm = &alarmStatus{}
+					alarms[m.AlarmID] = alarm
+				}
+
+				if alarm.LatestChangedAt.After(m.ChangedAt) {
+					log.Info("Update ignored because newer one exists")
+					continue
+				}
+				alarm.LatestChangedAt = m.ChangedAt
+
+				switch {
+				case alarm.Status != m.Status:
+					alarm.Status = m.Status
+					if alarm.Status == wire.StatusCleared {
+						log.Info(fmt.Sprintf("Status is %s, alarm won't be sent", alarm.Status))
+						alarm.ToSend = false
+					} else {
+						log.Info("Alarm triggered")
+						alarm.ToSend = true
+					}
+				case alarm.ToSend:
+					log.Info("Status hasn't changed, alarm was triggered earlier")
+				default:
+					log.Info("Status hasn't changed, alarm won't be sent")
+				}
+			case wire.SendAlarmDigest:
+				log := log.With(zap.Any("userID", m.UserID))
+
+				alarms := users[m.UserID]
+				if alarms == nil {
+					log.Info("No alarms for user, nothing to send")
+				}
+
+				active := &wire.AlarmDigest{
+					UserID: m.UserID,
+				}
+				var sent []*alarmStatus
+				for alarmID, alarm := range alarms {
+					if alarm.ToSend {
+						active.ActiveAlarms = append(active.ActiveAlarms, wire.Alarm{
+							AlarmID:         alarmID,
+							Status:          alarm.Status,
+							LatestChangedAt: alarm.LatestChangedAt,
+						})
+						sent = append(sent, alarm)
+					}
+				}
+
+				sort.Slice(active.ActiveAlarms, func(i int, j int) bool {
+					return active.ActiveAlarms[i].LatestChangedAt.Before(active.ActiveAlarms[j].LatestChangedAt)
+				})
+
+				tx <- active
+
+				for _, alarm := range sent {
+					alarm.ToSend = false
+				}
+
+				log.Info("Alarms sent", zap.Any("alarms", active))
+
+			default:
+				panic(fmt.Errorf("message of unknown type %T received", msg))
 			}
 		}
+		return ctx.Err()
 	}
 }
