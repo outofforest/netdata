42a5950e38d1d2add438d54b2ccf6866ef627493
Wojciech Małota-Wójcik <59281144+wojciech-malota-wojcik@users.noreply.github.com>
2021-11-03 16:11:57 +0100

    Move iocBuilder so it may be used in tests (#16)

--- cmd/main.go
+++ cmd/main.go
@@ -2,21 +2,9 @@ package main
 
 import (
 	"github.com/wojciech-malota-wojcik/netdata"
-	"github.com/wojciech-malota-wojcik/netdata/infra/sharding"
-
-	"github.com/wojciech-malota-wojcik/ioc"
-	"github.com/wojciech-malota-wojcik/netdata/infra"
-	"github.com/wojciech-malota-wojcik/netdata/infra/bus"
 	"github.com/wojciech-malota-wojcik/netdata/lib/run"
 )
 
-// iocBuilder configures IoC container
-func iocBuilder(c *ioc.Container) {
-	c.Singleton(infra.NewConfigFromCLI)
-	c.Transient(sharding.NewXORModuloIDGenerator)
-	c.Transient(bus.NewNATSConnection)
-}
-
 func main() {
-	run.Service("digest", iocBuilder, netdata.App)
+	run.Service("digest", netdata.IoCBuilder, netdata.App)
 }
